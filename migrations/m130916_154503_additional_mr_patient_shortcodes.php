9f96b3d931b214e4f9d9fca943294c5ca5594ca9
Mark <m@rkw.me>
2014-01-14 09:25:51 +0000

    removed references to models

--- migrations/m130916_154503_additional_mr_patient_shortcodes.php
+++ migrations/m130916_154503_additional_mr_patient_shortcodes.php
@@ -4,15 +4,39 @@ class m130916_154503_additional_mr_patient_shortcodes extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = EventType::model()->find('class_name=?',array('OphCoTherapyapplication'));
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array(":class_name" => "OphCoTherapyapplication"))->queryRow();
 
-		$event_type->registerShortcode('tdl','getLetterApplicationDiagnosisLeft','Therapy application diagnosis for left eye');
-		$event_type->registerShortcode('tdr','getLetterApplicationDiagnosisRight','Therapy application diagnosis for right eye');
+		$this->registerShortcode($event_type,'tdl','getLetterApplicationDiagnosisLeft','Therapy application diagnosis for left eye');
+		$this->registerShortcode($event_type,'tdr','getLetterApplicationDiagnosisRight','Therapy application diagnosis for right eye');
+		$this->registerShortcode($event_type,'ttl','getLetterApplicationTreatmentLeft','Therapy application treatment for left eye');
+		$this->registerShortcode($event_type,'ttr','getLetterApplicationTreatmentRight','Therapy application treatment for right eye');
+	}
 
-		$event_type->registerShortcode('ttl','getLetterApplicationTreatmentLeft','Therapy application treatment for left eye');
-		$event_type->registerShortcode('ttr','getLetterApplicationTreatmentRight','Therapy application treatment for right eye');
+	public function registerShortcode($event_type, $code, $method, $description)
+	{
+		if (!preg_match('/^[a-zA-Z]{3}$/',$code)) {
+			throw new Exception("Invalid shortcode: $code");
+		}
 
+		$default_code = $code;
 
+		if ($this->dbConnection->createCommand()->select("*")->from("patient_shortcode")->where("code = :code",array(":code" => strtolower($code)))->queryRow()) {
+			$n = '00';
+			while ($this->dbConnection->createCommand()->select("*")->from("patient_shortcode")->where("code = :code",array(":code" => 'z'.$n))->queryRow()) {
+				$n = str_pad((int) $n + 1, 2, '0', STR_PAD_LEFT);
+			}
+			$code = "z$n";
+
+			echo "Warning: attempt to register duplicate shortcode '$default_code', replaced with 'z$n'\n";
+		}
+
+		$this->insert('patient_shortcode',array(
+			'event_type_id' => $event_type['id'],
+			'code' => $code,
+			'default_code' => $default_code,
+			'method' => $method,
+			'description' => $description,
+		));
 	}
 
 	public function down()
@@ -22,15 +46,4 @@ class m130916_154503_additional_mr_patient_shortcodes extends CDbMigration
 		$this->delete('patient_shortcode','code = :sc',array(':sc'=>'ttl'));
 		$this->delete('patient_shortcode','code = :sc',array(':sc'=>'ttr'));
 	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
 }
--- migrations/m131023_100530_both_eye_shortcodes.php
+++ migrations/m131023_100530_both_eye_shortcodes.php
@@ -4,10 +4,10 @@ class m131023_100530_both_eye_shortcodes extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = EventType::model()->find('class_name=?',array('OphCoTherapyapplication'));
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array(":class_name" => "OphCoTherapyapplication"))->queryRow();
 
-		$event_type->registerShortcode('tdb','getLetterApplicationDiagnosisBoth','Therapy application diagnosis for both eyes');
-		$event_type->registerShortcode('ttb','getLetterApplicationTreatmentBoth','Therapy application treatment for both eyes');
+		$this->registerShortcode($event_type,'tdb','getLetterApplicationDiagnosisBoth','Therapy application diagnosis for both eyes');
+		$this->registerShortcode($event_type,'ttb','getLetterApplicationTreatmentBoth','Therapy application treatment for both eyes');
 	}
 
 	public function down()
@@ -16,14 +16,30 @@ class m131023_100530_both_eye_shortcodes extends CDbMigration
 		$this->delete('patient_shortcode','method = :meth',array(':meth'=>'getLetterApplicationDiagnosisBoth'));
 	}
 
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
+	public function registerShortcode($event_type, $code, $method, $description)
 	{
-	}
+		if (!preg_match('/^[a-zA-Z]{3}$/',$code)) {
+			throw new Exception("Invalid shortcode: $code");
+		}
 
-	public function safeDown()
-	{
+		$default_code = $code;
+
+		if ($this->dbConnection->createCommand()->select("*")->from("patient_shortcode")->where("code = :code",array(":code" => strtolower($code)))->queryRow()) {
+			$n = '00';
+			while ($this->dbConnection->createCommand()->select("*")->from("patient_shortcode")->where("code = :code",array(":code" => 'z'.$n))->queryRow()) {
+				$n = str_pad((int) $n + 1, 2, '0', STR_PAD_LEFT);
+			}
+			$code = "z$n";
+
+			echo "Warning: attempt to register duplicate shortcode '$default_code', replaced with 'z$n'\n";
+		}
+
+		$this->insert('patient_shortcode',array(
+			'event_type_id' => $event_type['id'],
+			'code' => $code,
+			'default_code' => $default_code,
+			'method' => $method,
+			'description' => $description,
+		));
 	}
-	*/
 }
