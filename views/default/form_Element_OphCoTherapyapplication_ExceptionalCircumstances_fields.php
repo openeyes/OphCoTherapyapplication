b232f52433e478c11fe643b0a5bb1b7605564daf
Mark Wadham <m@rkw.me>
2014-05-21 13:21:02 +0000

    fixed multiselect references

--- views/default/form_Element_OphCoTherapyapplication_ExceptionalCircumstances_fields.php
+++ views/default/form_Element_OphCoTherapyapplication_ExceptionalCircumstances_fields.php
@@ -138,7 +138,7 @@
 
 			echo $form->multiSelectList(
 				$element,
-				get_class($element) . '[' . $side . '_deviationreasons]',
+				$side . '_deviationreasons',
 				$side . '_deviationreasons',
 				'id',
 				CHtml::listData($element->getDeviationReasonsForSide($side),'id','name'),
@@ -289,5 +289,5 @@ foreach ($collections as $collection) {
 	$html_options['options'][(string) $collection->id] = array('data-order' => $collection->display_order);
 }
 */
-$form->multiSelectList($element, get_class($element) . '[' . $side . '_filecollections]', $side . '_filecollections', 'id', CHtml::listData($collections,'id','name'), array(), $html_options, false, false, null, false, false, array('label' => 4,'field' => 6));
+$form->multiSelectList($element, $side . '_filecollections', $side . '_filecollections', 'id', CHtml::listData($collections,'id','name'), array(), $html_options, false, false, null, false, false, array('label' => 4,'field' => 6));
 ?>
