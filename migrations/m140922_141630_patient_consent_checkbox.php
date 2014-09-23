ebaa0b5bc33b82acd578b59584b019d0a9dbba1c
Mark Wadham <m@rkw.me>
2014-09-23 14:53:42 +0100

    [OEM-227] made checkbox nullable

--- migrations/m140922_141630_patient_consent_checkbox.php
+++ migrations/m140922_141630_patient_consent_checkbox.php
@@ -4,8 +4,8 @@ class m140922_141630_patient_consent_checkbox extends CDbMigration
 {
 	public function up()
 	{
-		$this->addColumn('et_ophcotherapya_mrservicein','patient_sharedata_consent','tinyint(1) unsigned not null');
-		$this->addColumn('et_ophcotherapya_mrservicein_version','patient_sharedata_consent','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophcotherapya_mrservicein','patient_sharedata_consent','tinyint(1) unsigned null');
+		$this->addColumn('et_ophcotherapya_mrservicein_version','patient_sharedata_consent','tinyint(1) unsigned null');
 	}
 
 	public function down()
